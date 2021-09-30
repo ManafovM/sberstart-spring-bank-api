fa1c055ca0ef7e999811e2091355254a8fcf8bc2
Marif Manafov <manafov_m@bk.ru>
2021-09-30 16:37:34 +0700

    добавлены сущности

---
+++ src/main/java/bank/Customer.java
@@ -0,0 +1,30 @@
+package bank;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String firstName;
+    private String lastName;
+    private String role;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Account> accounts;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Card> cards;
+}
