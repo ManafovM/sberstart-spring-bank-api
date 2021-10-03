6481e0e43ab4649167ecaf240a6d775d170c11bf
Marif Manafov <manafov_m@bk.ru>
2021-10-03 20:28:32 +0700

    добавлены ограничения для полей сущностей

--- src/main/java/bank/entity/Account.java
+++ src/main/java/bank/entity/Account.java
@@ -1,15 +1,15 @@
 package bank.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 import java.util.List;
 
 @Data
+@ToString(exclude = "cards")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,12 +19,17 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotNull
+    @Pattern(regexp = "[0-9]{20}")
     private String number;
+
+    @NotNull
     private BigDecimal amount;
 
     @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
     private List<Card> cards;
 
+    @NotNull
     @ManyToOne()
     @JoinColumn(name = "customer_id")
     private Customer customer;
