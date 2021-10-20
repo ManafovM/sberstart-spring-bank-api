0581caf36e2e9bf78c6a4f905d9a10d1e5e8fc73
Marif Manafov <manafov_m@bk.ru>
2021-10-21 00:58:46 +0700

    добавлен javadoc

--- src/main/java/bank/service/CardService.java
+++ src/main/java/bank/service/CardService.java
@@ -5,7 +5,21 @@ import bank.dto.BalanceDto;
 import bank.dto.CardDto;
 
 public interface CardService extends GenericService<CardDto> {
+    /**
+     * Получает счет по id карты
+     *
+     * @param id идентификатор карты
+     * @return счет карты
+     */
     AccountDto getAccountByCardId(long id);
 
+
+    /**
+     * Вносит средства на счет карты
+     *
+     * @param id идентификатор карты
+     * @param balanceDto сумма для внесения
+     * @return баланс карты после внесения
+     */
     BalanceDto deposit(long id, BalanceDto balanceDto);
 }
--- src/main/java/bank/service/GenericService.java
+++ src/main/java/bank/service/GenericService.java
@@ -3,13 +3,41 @@ package bank.service;
 import java.util.List;
 
 public interface GenericService<T> {
+    /**
+     * Получает сущность типа T по id
+     *
+     * @param id идентификатор сущности
+     * @return сущность
+     */
     T getById(long id);
 
+    /**
+     * Получает все сущности типа T
+     *
+     * @return список сущностей
+     */
     List<T> getAll();
 
+    /**
+     * Создает сущность типа T
+     *
+     * @param entity новая сущность
+     * @return созданная сущность
+     */
     T create(T entity);
 
+    /**
+     * Обновляет сущность типа T
+     *
+     * @param entity новая сущность
+     * @return обновленная сущность
+     */
     T update(T entity);
 
+    /**
+     * Удаляет сущность типа T
+     *
+     * @param entity удаляемая сущность
+     */
     void delete(T entity);
 }
