bf2d9d705e72794cc9bb14680967e3daa012060f
Manafov Marif <manafov_m@bk.ru>
2021-10-21 13:38:20 +0700

    Dev (#25)
    * инициализация spring приложения
    
    * добавлены сущности
    
    * добавлена java конфигурация spring
    
    * добавлена java конфигурация spring
    
    * добавлены dao
    
    * исправлены dao, добавлен PersistenceJPAConfig
    
    * настроена конфигурация приложения
    
    * dao с дженериками
    
    * dao с дженериками
    
    * добавлены сервисы для customer
    
    * добавлен контроллер для customer
    
    * добавлены ограничения для полей сущностей
    
    * добавлен скрипт заполнения таблиц бд
    
    * service с дженериками
    
    * настроен liquibase
    
    * исправлены сервисы
    
    * исправлены сущности
    
    * добавлен CustomerDto
    
    * добавлен CustomerDto
    
    * добавлены сервисы и контроллеры для Account и Card
    
    * добавлены методы в AccountController
    
    * добавлены ссылки на доступные ресурсы
    
    * добавлены методы для внесения средств и отображения баланса
    
    * созданы конвертеры для dto
    
    * созданы интеграционные тесты
    
    * созданы интеграционные тесты
    
    * добавлен скрипт для создания таблиц
    
    * рефакторинг
    
    * добавлены поля version в сущности
    
    * добавлен javadoc
    
    * рефакторинг
    
    * добавлены not null ограничения

--- src/main/resources/liquibase/010-create-tables.sql
+++ src/main/resources/liquibase/010-create-tables.sql
@@ -4,20 +4,20 @@ create table CUSTOMER
 (
     ID        BIGINT auto_increment
         primary key,
-    FIRSTNAME VARCHAR(255),
-    LASTNAME  VARCHAR(255),
-    ROLE      VARCHAR(255),
-    VERSION BIGINT
+    FIRSTNAME VARCHAR(255) NOT NULL,
+    LASTNAME  VARCHAR(255) NOT NULL,
+    ROLE      VARCHAR(255) NOT NULL,
+    VERSION BIGINT NOT NULL
 );
 
 create table ACCOUNT
 (
     ID          BIGINT auto_increment
         primary key,
-    AMOUNT      DECIMAL(19, 2),
-    NUMBER      VARCHAR(255),
-    CUSTOMER_ID BIGINT,
-    VERSION BIGINT,
+    AMOUNT      DECIMAL(19, 2) NOT NULL,
+    NUMBER      VARCHAR(255) NOT NULL,
+    CUSTOMER_ID BIGINT NOT NULL,
+    VERSION BIGINT NOT NULL,
     constraint FK_CUSTOMER_ID
         foreign key (CUSTOMER_ID) references CUSTOMER (ID)
 );
@@ -26,12 +26,12 @@ create table CARD
 (
     ID         BIGINT auto_increment
         primary key,
-    CVC        VARCHAR(255),
-    EXPIREDATE VARCHAR(255),
-    NUMBER     VARCHAR(255),
-    STATUS     VARCHAR(255),
-    ACCOUNT_ID BIGINT,
-    VERSION BIGINT,
+    CVC        VARCHAR(255) NOT NULL,
+    EXPIREDATE VARCHAR(255) NOT NULL,
+    NUMBER     VARCHAR(255) NOT NULL,
+    STATUS     VARCHAR(255) NOT NULL,
+    ACCOUNT_ID BIGINT NOT NULL,
+    VERSION BIGINT NOT NULL,
     constraint FK_ACCOUNT_ID
         foreign key (ACCOUNT_ID) references ACCOUNT (ID)
 );
